3b02312e806f1a72bfd9a0dd32ca176eab419d63
gourav kumar <gourav.kumar@oneguardian.in>
2025-12-31 14:00:26 +0530

    Added the Lambark repo

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,11 +7,14 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 import utils.ExtentManager;
+
+import java.time.Duration;
 
 public class BaseTest {
     public static WebDriver driver;
@@ -26,25 +29,48 @@ public class BaseTest {
 
 
     @BeforeMethod
-    public void setUp(){
+    public void setUp() {
+
         WebDriverManager.chromedriver().setup();
-        driver= new ChromeDriver();
+
+        // 🔹 Read headless value (default = false)
+        String headlessValue = System.getProperty("headless", "false");
+        boolean isHeadless = headlessValue.equalsIgnoreCase("true");
+
+        ChromeOptions options = new ChromeOptions();
+
+        if (isHeadless) {
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            LOGGER.info("Running tests in HEADLESS mode");
+        } else {
+            LOGGER.info("Running tests in UI (Browser) mode");
+        }
+
+        driver = new ChromeDriver(options);
         LOGGER.info("Launched ChromeDriver instance");
-        driver.manage().window().maximize();
-        // set page load timeout and small implicit wait as safety net for legacy operations
+
+        if (!isHeadless) {
+            driver.manage().window().maximize();
+        }
+
         try {
-            driver.manage().timeouts().pageLoadTimeout(java.time.Duration.ofSeconds(30));
-            driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
-            driver.manage().timeouts().scriptTimeout(java.time.Duration.ofSeconds(20));
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+            driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(20));
         } catch (Exception ignored) {}
 
-        // start from a clean session
-        try { driver.manage().deleteAllCookies(); } catch (Exception ignored) {}
+        try {
+            driver.manage().deleteAllCookies();
+        } catch (Exception ignored) {}
 
         driver.get("https://bellavitaorganic.com/");
         LOGGER.info("Navigated to base URL");
-
     }
+
 
     @AfterMethod
     public void tearUp(){
