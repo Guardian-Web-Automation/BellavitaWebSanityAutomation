525d141447861fccb9a65885cf61b6945071e6d6
gourav kumar <gourav.kumar@oneguardian.in>
2025-12-31 16:40:18 +0530

    some changes

--- src/test/java/utils/ExtentManager.java
+++ src/test/java/utils/ExtentManager.java
@@ -7,7 +7,6 @@ import com.aventstack.extentreports.reporter.configuration.Theme;
 import java.util.TimeZone;
 
 public class ExtentManager {
-
     private static ExtentReports extent;
 
     public static ExtentReports getInstance() {
@@ -18,22 +17,17 @@ public class ExtentManager {
     }
 
     public static ExtentReports createInstance(String fileName) {
-
-        // ✅ Force IST timezone (VERY IMPORTANT)
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
-
         ExtentSparkReporter reporter = new ExtentSparkReporter(fileName);
         reporter.config().setDocumentTitle("Bellavita Organic Automation Test Report");
-        reporter.config().setReportName("Regression Suite Test Results");
+        reporter.config().setReportName("Regression suite Test Results");
         reporter.config().setTheme(Theme.STANDARD);
 
-        ExtentReports extent = new ExtentReports();
+        extent = new ExtentReports();
         extent.attachReporter(reporter);
-
-        extent.setSystemInfo("Framework", "Selenium Java + TestNG");
-        extent.setSystemInfo("Author", "Gourav Kumar");
+        extent.setSystemInfo("Framework", "Selenium Java TestNG");
+        extent.setSystemInfo("Author", "Gaurav Kumar");
         extent.setSystemInfo("Browser", "Chrome");
-        extent.setSystemInfo("Timezone", "Asia/Kolkata (IST)");
 
         return extent;
     }
